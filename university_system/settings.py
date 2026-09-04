45eaa6e84eced997a7f17e19e5eee4604c84d373
Piyushnikam2005 <piyushnikam17@gmail.com>
2026-09-04 11:35:00 +0530

    added

--- university_system/settings.py
+++ university_system/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +21,6 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "web-production-4e210.up.railway.app",
 ]
 
 TEMPLATES = [
@@ -123,8 +121,4 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-4e210.up.railway.app",
 ]
